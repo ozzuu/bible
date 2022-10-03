58503fe6a3ec41644a7b3e78aa2168b268fa697f
thisago <thiago@oxyoy.com>
2022-10-03 11:10:40 +0000

    updated backup script

--- scripts/backupAllDb.sh
+++ scripts/backupAllDb.sh
@@ -5,11 +5,13 @@ serverDir=$2
 allDbDir=db
 serverBackupDir=$allDbDir/backups
 allDb=all.db
+bakName="$(date '+%Y-%m-%d').db"
 
 echo "SSH server (user@server): $server"
 echo "SSH server working dir: $serverDir"
 echo "Server's backup dir: $serverBackupDir"
 echo "Server's all db: $allDbDir/$allDb"
+echo "Backup file name: $bakName"
 
 echo -ne "\nCorrect (press enter)? "; read correct
 if [ "$correct" != "" ]; then
@@ -25,7 +27,9 @@ cd $serverDir &&
 echo -e \"\tSetup directories and files\" &&
 mkdir --parents \"$serverBackupDir\" &&
 echo -e \"\tCopying the DB\" &&
-cp \"$allDbDir/$allDb\" \"$serverBackupDir/$(date '+%Y-%m-%d').db\" &&
+cp \"$allDbDir/$allDb\" \"$serverBackupDir/$bakName\" &&
+echo -e \"\tCopied! Listing all backups\" &&
+ls \"$serverBackupDir\"
 echo -e \"\tFinished\"
 "
 
